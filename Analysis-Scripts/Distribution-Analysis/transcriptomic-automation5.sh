3786402c56674fb5f299e397fb78885e067bce08
Michael Scott Bradshaw <mbrad94@m8int02.fsl.byu.edu>
2019-01-07 12:36:40 -0700

    finished the running the analysies of cna and transcriptomics

--- Analysis-Scripts/Distribution-Analysis/transcriptomic-automation5.sh
+++ Analysis-Scripts/Distribution-Analysis/transcriptomic-automation5.sh
@@ -3,6 +3,6 @@ module load anaconda
 pip install --user pandas==0.23.0
 pip install --user mkl
 for i in `seq 81 100`; do
-    python3 Classification-first-and-second-general-use.py '../../Data/Distribution-Data-Set/train_transcriptomics_distribution'$i'.csv' '../../Data/Distribution-Data-Set/test_transcriptomics_distribution'$i'.csv' 'transcriptomics_results'$i'.csv'
+    python3 Classification-first-and-second-general-use.py '../../Data/Distribution-Data-Set/Transcriptomics-100/train_transcriptomics_distribution'$i'.csv' '../../Data/Distribution-Data-Set/Transcriptomics-100/test_transcriptomics_distribution'$i'.csv' 'TranscriptomicsResults/transcriptomics_results'$i'.csv'
 
 done
