e5ab9fa98705bb28e1f63e4b6c5133a923e56ff9
MSBradshaw <michaelscottbradshaw@gmail.com>
2019-01-07 14:35:04 -0700

    created plots of the 100 transcriptomics and cna datasets

--- Analysis-Scripts/Distribution-Analysis/cna-plotting-automation.sh
+++ Analysis-Scripts/Distribution-Analysis/cna-plotting-automation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in `seq 1 100`; do
-    python3 results-plotter.py 'transcriptomic_results/transcriptomics_results'$i'.csv' 'transcriptomic-results-plot'$i'.png'
+    python3 general-use-result-plotter.py 'CNAResults/cna_results'$i'.csv' 'CNAResults/cna-results-plot'$i'.png' 'Resampling Test CNA Accuracy'
 done
--- Analysis-Scripts/Distribution-Analysis/transcriptomic-plotting-automation.sh
+++ Analysis-Scripts/Distribution-Analysis/transcriptomic-plotting-automation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in `seq 1 100`; do
-    python3 results-plotter.py 'transcriptomic_results/transcriptomics_results'$i'.csv' 'transcriptomic-results-plot'$i'.png'
+    python3 results-plotter.py 'TranscriptomicsResults/transcriptomics_results'$i'.csv' 'TranscriptomicsResults/transcriptomic-results-plot'$i'.png'
 done
