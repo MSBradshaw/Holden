6f39f8950959110c72224bbbbc96ce99e69d7c16
MSBradshaw <michaelscottbradshaw@gmail.com>
2019-01-16 09:10:04 -0700

    editted the imputation.R script

--- Phony-Scripts/imputation.R
+++ Phony-Scripts/imputation.R
@@ -5,11 +5,22 @@ library(pamr)
 library(missForest)
 library(glmnet)
 library(DreamAI)
+library(stringr)
+
 set.seed(0)
+args = commandArgs(trailingOnly=TRUE)
 
-#setwd('C:/Users/Michael/Documents/Holden')
+setwd('C:/Users/Michael/Documents/Holden2')
+file_path = 'Data/Data-Uncompressed-Original/CNA.cct'
 
-cna <- read_tsv('../Data/Data-Uncompressed-Original/CNA.cct')
+#file_path = args[1]
+#index = as.numeric(args[2])
+#start_pos = as.numeric(args[3])
+
+file_path = 'Data/Data-Uncompressed-Original/CNA.cct'
+
+
+info <- read_tsv(file_path)
 #get the protein names
 names <- unlist(info['idx'])
 #remove the row names
@@ -17,32 +28,44 @@ info <- info[,2:ncol(info)]
 #transpose the data
 info <- as.tibble(t(info))
 
+index = 1
+start_pos = (ncol(info)-50)
+
 #create person
-create.person <- function(data,index){
+create.person <- function(data,row,start,output_name){
   #create a new row which is a copy of row indicated by the index
-  data[(nrow(data)+1),] <- data[index,]
+  data[(nrow(data)+1),] <- data[row,]
   #impute 100 at a time
   #seq(1,(ncol(data)/100))
-  for(i in seq(1,(ncol(data)),100)){
-    #replace i through i + 100 with NA
-    if((i+100) > ncol(data)){
-      end <- ncol(data)
-    }else{
-      end <- i+100
-    }
-    data[nrow(data),i:(i+100)] <- NA
-    output <- DreamAI(data)
-    print('Printing Data')
-    print(typeof(data))
-    print('Printing what was returned')
-    print(typeof(output))
+  print(-1)
+  if(start + 100 > ncol(data)) {
+    end = ncol(data)
+  }else{
+    end = start + 100
   }
+  data[nrow(data),start:end] <- 7
+  #DreamAI(data)
+  print(0)
+  data_small <- data[nrow(data),]
+  #replace everything before start
+  print(1)
+  if(start != 1){
+    data_small[1,start:1]  <- NA
+  }
+  print(2)
+  #replace everything after start
+  if(end < ncol(data_small) ){
+    data_small[1,end:ncol(data_small)]  <- NA
+  }
+  print(3)
+  data_small['row'] <- c(index)
+  print(4)
+  write_csv(data_small,output_name)
+  print(5)
+  return(data_small)
 }
 
-create.person(info,as.numeric(args[2]))
 
-
-
-
-
-
+data_set = gsub('\\.\\w+','',str_extract(file_path,'\\w*\\.\\w+'))
+outfile = paste(as.character(index),'-',as.character(start_pos),'-',as.character(data_set),'.csv',sep='')
+a= create.person(info,index,start_pos,outfile)
