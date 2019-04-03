527a9d8201107b3ff7a2fa84ba7dd4959958fa01
Michael Scott Bradshaw <mbrad94@m7int01.fsl.byu.edu>
2019-04-03 08:22:27 -0600

    rand analysis on the full feature data for all 3 types

---
+++ Analysis-Scripts/Generic-Analysis/random-cna.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH --time=10:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=8192M   # memory per CPU core
+#SBATCH --mail-user=Michaelscottbradshaw@gmail.com   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+# Compatibility variables for PBS. Delete if not needed.
+export PBS_NODEFILE=`/fslapps/fslutils/generate_pbs_nodefile`
+export PBS_JOBID=$SLURM_JOB_ID
+export PBS_O_WORKDIR="$SLURM_SUBMIT_DIR"
+export PBS_QUEUE=batch
+
+# Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+
+module load anaconda
+vals=($(seq -s ' ' 1 50))
+for i in "${vals[@]}"
+do
+    echo  cna-random-$i
+    python Classification-full-feature.py ../../Data/Random-Data-Set/CNA-100/train_cna_random$i.csv ../../Data/Random-Data-Set/CNA-100/test_cna_random$i.csv random-cna-50-ff/random-cna-results-ff-$i.csv cna
+done
+tar -zcvf random-cna-50-ff.tar.gz random-cna-50-ff/ 
