9f527b07b880bdbb8bf8b80da4f179c1ef3efa9e
Michael Bradshaw <michael@engr2-4-205-dhcp.int.colorado.edu>
2019-10-10 15:49:21 -0600

    created figure of feature downsampling sensitivity results

---
+++ Analysis-Scripts/Feature-Sensitivity-Analysis/feature_sensitivity_plotter.R
@@ -0,0 +1,90 @@
+library(readr)
+library(ggplot2)
+library(dplyr)
+
+data <- read_csv('/Users/michael/Holden/Analysis-Scripts/Feature-Sensitivity-Analysis/results.csv')
+
+std_error <- function(x){
+  sd(x)/sqrt(length(x))
+}
+
+get_errors <- function(x){
+  #x <- rf
+  errors = c()
+  for(i in unique(x$features)){
+    d <- x[x$features==i,]$score
+    n <- std_error(d)
+    errors <- c(errors,n)
+  }
+  errors
+}
+
+rf <- data[data$learner == 'Random Forest',]
+errors <- get_errors(rf)
+rf <- aggregate(rf,by=list(rf$features),FUN=mean)
+rf$learner <- 'Random Forst'
+rf$type <- 'Impuation'
+rf$error <- errors
+
+gb <- data[data$learner == 'GBC',]
+errors <- get_errors(gb)
+gb <- aggregate(gb,by=list(gb$features),FUN=mean)
+gb$learner <- 'GBC'
+gb$type <- 'Impuation'
+gb$error <- errors
+
+nb <- data[data$learner == 'Naive Bayes',]
+errors <- get_errors(nb)
+nb <- aggregate(nb,by=list(nb$features),FUN=mean)
+nb$learner <- 'Naive Bayes'
+nb$type <- 'Impuation'
+nb$error <- errors
+
+knn <- data[data$learner == 'KNN',]
+errors <- get_errors(knn)
+knn <- aggregate(knn,by=list(knn$features),FUN=mean)
+knn$learner <- 'KNN'
+knn$type <- 'Impuation'
+knn$error <- errors
+
+d <- bind_rows(knn,nb,gb,rf)
+
+d$charFeatures <- factor(as.character(d$features),level=c('100','500','1000','2000','4000','6000','8000','10000','12000','14000','16000','17000'))
+
+number <- c('100','500','1000','2000','4000','6000','8000','10000','12000','14000','16000','17000')
+
+d$size = apply(d,1,function(row){
+  groups <- c(100,500,1000,2000,4000,6000,8000,10000,12000,14000,16000,17000)
+  #groups <- c('100','500','1000','2000','4000','6000','8000','10000','12000','14000','16000','17000')
+  i <- match(as.numeric(row[6]),groups)
+  i
+})
+
+data$charFeatures <- factor(as.character(data$features),level=c('100','500','1000','2000','4000','6000','8000','10000','12000','14000','16000','17000'))
+
+# TODO added error bars to the last graph
+
+ggplot(data = data,aes(x=charFeatures,y=score,color=learner))  + geom_boxplot()
+ggsave('/Users/michael/Holden/Analysis-Scripts/Feature-Sensitivity-Analysis/results.png',units='in',width=11,height=8.5)
+
+ggplot(data = d,aes(x=charFeatures,y=score,color=learner))  + geom_point()
+ggsave('/Users/michael/Holden/Analysis-Scripts/Feature-Sensitivity-Analysis/results-point.png',units='in',width=11,height=8.5)
+
+ggplot(data = d,aes(x=charFeatures,y=score,color=learner))  + geom_point() + geom_errorbar(aes(ymin=score-error, ymax=score+error),width=.2)
+ggsave('/Users/michael/Holden/Analysis-Scripts/Feature-Sensitivity-Analysis/results-point-errorbar.png',units='in',width=11,height=8.5)
+
+ggplot(data = d,aes(x=size,y=score,color=learner))  + geom_point() + 
+  geom_errorbar(aes(ymin=score-error, ymax=score+error),width=.2) + 
+  xlab('number of features') +
+  geom_line() + scale_x_continuous(breaks = 1:12,
+                                   labels = c('100','500','1000','2000','4000','6000','8000','10000','12000','14000','16000','17000'))
+ggsave('/Users/michael/Holden/Analysis-Scripts/Feature-Sensitivity-Analysis/results-point-errorbar-line.png',units='in',width=11,height=8.5)
+
+ggplot(data = d,aes(x=features,y=score,color=learner))  + geom_line()
+ggsave('/Users/michael/Holden/Analysis-Scripts/Feature-Sensitivity-Analysis/results-line.png',units='in',width=11,height=8.5)
+
+ggplot(data = d,aes(x=log(features),y=score,color=learner))  + geom_line() + geom_point()
+ggsave('/Users/michael/Holden/Analysis-Scripts/Feature-Sensitivity-Analysis/results-line-log.png',units='in',width=11,height=8.5)
+
+ggplot(data = d,aes(x=log(features),y=score,color=learner))  + geom_line() + geom_point() + geom_errorbar(aes(ymin=score-error, ymax=score+error))
+ggsave('/Users/michael/Holden/Analysis-Scripts/Feature-Sensitivity-Analysis/results-line-log_errorbar.png',units='in',width=11,height=8.5)
